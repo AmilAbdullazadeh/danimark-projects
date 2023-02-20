5efb76385b66848846fdeebf75d8cfce8c357298
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2023-02-20 19:59:21 +0400

    new

---
+++ js-projects-denmark/modal-popup/js/main.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const openBtn = document.querySelector(".btn-open")
+const closeBtn = document.querySelector(".btn-close")
+
+const openPopup = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closePopup = () => {
+    modal.classList.add("hidden")
+    overlay.classList.add("hidden")
+}
+
+openBtn.addEventListener("click", openPopup)
+closeBtn.addEventListener("click", closePopup)
+overlay.addEventListener("click", closePopup)
